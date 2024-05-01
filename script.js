212052714078bfa9566e09e7614b959c8fec7d82
MIDHUN M <135270320+MIDHUNAVATHAR@users.noreply.github.com>
2024-05-01 17:40:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//*Form Validation*/
+// document.getElementById("submitbtn").addEventListener("click",validateForm); 
+// function validateForm(){
+//     const x=document.getElementsByTagName("input");
+//     for(let i=0;i<x.length;i++){
+//         if(x[i].value==""){
+//             alert("Please Fill all the fields ");
+//             return false;
+//         }
+//     }
+// }
+
+
+
+
+
